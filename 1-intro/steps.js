65fd2c2401d104744aa5cee4ed785f5e94315e51
Timothy Pew <mrlasers@mrlasers.com>
2022-07-20 14:37:38 -0700

    add intro exercises

---
+++ 1-intro/steps.js
@@ -0,0 +1,61 @@
+// # TYPES
+
+// print "Hello, World!" to console
+
+// comment out the previous command
+
+// print a string to the console
+
+// print a number to the console
+
+// print a boolean to the console
+
+// print an undefined to the console
+
+// print a null to the console
+
+// print an object to the console
+
+// use a multiline comment to comment out all of the previous commands
+
+// # STRINGS
+
+// print the length of "Hello, World!" to the console
+
+// concatenate two strings together to create the string "Hello, World!" and print it to the console
+
+// print "Hello, World!" to the console so that it appears in all capital letters ("HELLO, WORLD!")
+
+// print "Hello, World!" to the console so that it appears in all lowercase letters ("hello, world!")
+
+// use the startsWith('Hello') method on a string so that the result is true and print it to the console
+
+// use the startsWith('Hello') method on a string so that the result is false and print it to the console
+
+// # NUMBERS
+
+// print 23 to the console using the addition operator and any two numbers
+
+// print 7 to the console using the subtraction operator and any two numbers
+
+// print 30 to the console using the multiplication operator and any two numbers
+
+// print 10 to the console using the division operator and any two numbers
+
+// print the sum of 2 and 3 to the console
+
+// print the product of 6 and 5 to the console
+
+// multiply the sum of 2 and 4 by 5 to get 30 and print it to the console
+
+// multiply two numbers together to get the result 5 and print it to the console
+
+// use the modulo operator on two numbers so that the result is 0 (zero) and print it to the console
+
+// # MATH
+
+// use a method of the Math object to round 23.7 down to the next lowest integer value and print the result to the console
+
+// use a method of the Math object to round 23.7 up to the next highest integer value and print it to the console
+
+// print a random number to the console
