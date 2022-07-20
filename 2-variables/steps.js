8e08d990bb3093f7a178d133d06556145ad31591
Timothy Pew <mrlasers@mrlasers.com>
2022-07-20 14:58:19 -0700

    add variables

---
+++ 2-variables/steps.js
@@ -0,0 +1,55 @@
+// create a variable named "bob" that can't be reassigned and give it the value "Robert"
+
+// print bob to the console
+
+// create a variable named "nameTag" that can be reassigned and give it the value "Bob"
+
+// print nameTag to the console
+
+// change the value of the nameTag variable to the value of the bob variable
+
+// print nameTag to the console again
+
+// # NUMBERS
+
+// create a variable named "magicNumber" with let and set it equal to 5
+
+// print magicNumber to the console
+
+// change the value of magicNumber to 23
+
+// print magicNumber to the console again
+
+// change the value of magicNumber to 22 using the decrement operator
+
+// print magicNumber to the console again
+
+// change the value of magicNumber back to 23 using the increment operator
+
+// print the magicNumber to the console again
+
+// change the value of magicNumber to 46 using an assignment operator
+
+// print magicNumber to the console again
+
+// # STRINGS
+
+// create a variable named helloMagicNumber with the value of "Hello, the magic number is 46!" using a template literal string
+
+// print helloMagicNumber to the console
+
+// create a variable to hold a magic number that you think Bob might like
+
+// using a template literal string, create a variable with the message "Bob's magic number is X", where "X" is Bob's magic number
+
+// print Bob's magic number message to the console
+
+// print the data type of Bob's magic number to the console
+
+// print the data type of Bob's magic number message to the console
+
+// print the data type of the undefined value to the console (what do you think it will be?)
+
+// print the data type of the null value to the console (what do you think this will be?)
+
+// using the string concatenation operator, create the sentence "The magicNumber is '46' and its type is 'number', the value of null is 'null' and its type is 'object'"
